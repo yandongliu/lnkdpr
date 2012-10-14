995950f97176995d40945f27c2cccce8ec93e765
Rock Liu <askjupiter@gmail.com>
2012-10-14 20:30:34 +0000

    join_pr_prof.py

--- join_pr_prof.py
+++ join_pr_prof.py
@@ -14,17 +14,21 @@ with open(fn_prof,'r') as f:
       profile={}
       x[current_id]=profile
     else:
-      cols=l.split(':')
+      cols=l.split(':',1)
       profile[cols[0]]=cols[1]
 
 #for c_id in x:
   #print c_id,x[c_id]
 with open(fn_pr,'r') as f:
+  cnt=0
   for l in f:
+    cnt+=1
     l=l.strip()
     cols=l.split(' ')
     if cols[0] in x:
       p=x[cols[0]]
-      print 'id:%s\tname:%s\theadline:%s\tcurrent_pos:%s\tscore:%s'%(cols[0],p['name'],p['headline'],p['current position'],cols[1])
+      id1=cols[0]
+      print '%d\tid:%s\tname:%s\theadline:%s\tcurrent_pos:%s\tscore:%s'%(cnt,cols[0],p['name'],p['headline'],p['current position'],cols[1])
+      #print 'id:<a href="data/%s/%s/%s">%s</a>\tname:%s\theadline:%s\tcurrent_pos:%s\tscore:%s'%(cols[0],p['name'],p['headline'],p['current position'],cols[1])
     else:
-      print 'id:%s\tname:%s\theadline:%s\tcurrent_pos:%s\tscore:%s'%(cols[0],'-','-','-',cols[1])
+      print '%d\tid:%s\tname:%s\theadline:%s\tcurrent_pos:%s\tscore:%s'%(cnt,cols[0],'-','-','-',cols[1])
