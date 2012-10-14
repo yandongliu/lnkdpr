ba886ace912f68fa0c2d3505b6623da570051bc6
Rock Liu <jerrya.liu@aol.com>
2012-10-14 21:02:58 +0000

    updated pr.graph profiles.txt

--- parse_lnkd_profiles.py
+++ parse_lnkd_profiles.py
@@ -2,10 +2,16 @@ import sys
 import re
 import os
 
+def uniq_list(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if x not in seen and not seen_add(x)]
+
 def parse(fn):
   name=None
   headline=None
   current_pos=None
+  companies=[]
   with open(fn,'r') as f:
     content=f.read()
 
@@ -21,25 +27,37 @@ def parse(fn):
     if m:
       current=p_tag.sub('',m[0].replace('\n',' '))
       current_pos=current.replace('  ',' ').strip()
-  return name,headline,current_pos
+
+    m=p_company.findall(content)
+    if m:
+      #print m
+      companies=uniq_list(reversed(m))
+    #m=p_company2.findall(content)
+    #if m:
+      #print m
+  return name,headline,current_pos,companies
     
 p_headline=re.compile('<p class="headline-title title" style="display:block">(.*?)</p>',re.MULTILINE|re.DOTALL)
 p_name=re.compile('<span class="full-name"><span class="given-name">(.*?)</span> <span class="family-name">(.*?)</span></span>')
 p_current=re.compile('<ul class="current">\n*<li>(.*?)\n*</li>',re.MULTILINE|re.DOTALL)
 str_current='<ul class="current">\n*<li>(.*?)\n*</li>'
 p_tag = re.compile(r'<[^>]+>')
+p_company=re.compile(r'<a class="company-profile-public" href="[^"]+"><span class="org summary">(.+)</span></a>')
+#p_company2=re.compile(r'<a class="company-profile-public" href="[^"]+"><[^>]*>(.+)</span></a>')
 data_dir='/home/ubuntu/linkedin/data/'
 #view_dir='./view/'
 
 cnt=0
 
 for subdir in os.listdir(data_dir):
-  for fn in os.listdir(data_dir+subdir):
-    name,hl,current_pos=parse(data_dir+subdir+'/'+fn)
-    print 'id:%s'%fn.split('__')[4].split('?')[0]
-    print 'name:%s'%name
-    print 'headline:%s'%hl
-    print 'current position:%s'%current_pos
-    cnt+=1
-    if cnt%1000==0:
-      print >>sys.stderr, 'processed %d profiles'%cnt
+  for subdir2 in os.listdir(data_dir+subdir):
+    for fn in os.listdir(data_dir+subdir+'/'+subdir2):
+      name,hl,current_pos,companies=parse(data_dir+subdir+'/'+subdir2+'/'+fn)
+      print 'id:%s'%fn.split('__')[4].split('?')[0]
+      print 'name:%s'%name
+      print 'headline:%s'%hl
+      print 'current position:%s'%current_pos
+      print 'companies:%s'%':'.join(companies)
+      cnt+=1
+      if cnt%1000==0:
+        print >>sys.stderr, 'processed %d profiles'%cnt
